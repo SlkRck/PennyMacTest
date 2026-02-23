7fd1b4cec046e8bb6380d9843d38d656bd058c91
Richard Taylor <azdawgster@gmail.com>
2026-02-23 07:48:17 -0800

    multiple fixes in network module

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -44,11 +44,11 @@ resource "aws_security_group" "vpce" {
   }
 
   egress {
-    description = "All egress"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS to VPC CIDR"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
   }
 
   tags = merge(var.tags, { Name = "${var.name_prefix}-vpce-sg" })
@@ -92,3 +92,61 @@ resource "aws_vpc_endpoint" "sts" {
 }
 
 data "aws_region" "current" {}
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.this.id
+
+  # no ingress/egress blocks means "remove all rules"
+  tags = merge(var.tags, { Name = "${var.name_prefix}-default-sg" })
+}
+resource "aws_cloudwatch_log_group" "vpc_flow" {
+  name              = "/aws/vpc/${var.name_prefix}/flow-logs"
+  retention_in_days = 30
+  tags              = var.tags
+}
+
+data "aws_iam_policy_document" "flow_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "flow" {
+  name               = "${var.name_prefix}-vpc-flowlogs-role"
+  assume_role_policy = data.aws_iam_policy_document.flow_assume.json
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "flow_logs" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams"
+    ]
+    resources = ["${aws_cloudwatch_log_group.vpc_flow.arn}:*"]
+  }
+}
+
+resource "aws_iam_role_policy" "flow" {
+  name   = "${var.name_prefix}-vpc-flowlogs"
+  role   = aws_iam_role.flow.id
+  policy = data.aws_iam_policy_document.flow_logs.json
+}
+
+resource "aws_flow_log" "this" {
+  vpc_id               = aws_vpc.this.id
+  traffic_type         = "ALL"
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc_flow.arn
+  iam_role_arn         = aws_iam_role.flow.arn
+  tags                 = var.tags
+}
+
